6a95154b209528eb087a5783d0273d9d6e46316f
home-pc\maxim <suhinin.maxim@gmail.com>
2018-07-12 19:28:21 +0300

    ADD: Payments test

--- app/src/main/java/com/childaplic/mosaic/ui/payment/PaymentActivity.java
+++ app/src/main/java/com/childaplic/mosaic/ui/payment/PaymentActivity.java
@@ -3,20 +3,27 @@ package com.childaplic.mosaic.ui.payment;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.childaplic.mosaic.R;
 import com.childaplic.mosaic.services.imageloader.ImageLoaderService;
 import com.childaplic.mosaic.services.payments.GooglePayUtils;
 import com.childaplic.mosaic.services.payments.PaymentsService;
+import com.childaplic.mosaic.ui.common.Margin;
 import com.childaplic.mosaic.ui.common.Size;
 import com.childaplic.mosaic.utils.LayoutHelper;
 import com.childaplic.mosaic.utils.ScreenUtil;
@@ -30,6 +37,9 @@ import com.google.android.gms.wallet.PaymentDataRequest;
 import com.google.android.gms.wallet.PaymentMethodToken;
 import com.google.android.gms.wallet.PaymentsClient;
 import com.google.android.gms.wallet.TransactionInfo;
+
+import java.util.Currency;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -56,6 +66,10 @@ public class PaymentActivity extends DaggerAppCompatActivity {
     // region Fields
 
     private PaymentsClient mPaymentsClient;
+    private Size mBtnPaySize;
+    private Margin mBtnPayMargin;
+    private Size mPigSize;
+    private Size mDescriptionSize;
 
     // endregion
 
@@ -72,6 +86,8 @@ public class PaymentActivity extends DaggerAppCompatActivity {
 
     // region View Components
 
+    private View mDecorView;
+
     private FrameLayout mRootView;
     private ImageView mImagePig;
     private ImageView mImageDescription;
@@ -87,6 +103,7 @@ public class PaymentActivity extends DaggerAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(createView());
 
+        mDecorView = getWindow().getDecorView();
         mPaymentsClient = GooglePayUtils.createPaymentsClient(this);
 
         mBtnPay.setEnabled(true);
@@ -99,8 +116,26 @@ public class PaymentActivity extends DaggerAppCompatActivity {
             case LOAD_PAYMENT_DATA_REQUEST_CODE:
                 boolean isSuccess = handleResult(resultCode, data);
                 mPaymentsService.onResult(isSuccess);
-                finish();
+                if (isSuccess) {
+                    finish();
+                }
                 break;
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (hasFocus) {
+                mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
         }
     }
 
@@ -111,12 +146,50 @@ public class PaymentActivity extends DaggerAppCompatActivity {
 
     private View createView() {
         mRootView = new FrameLayout(this);
+
+        calculatePayBtnSize();
+        calculatePigWidth();
+        calculateDescriptionSize();
+        calculatePayBtnMargin();
+
         addBackground();
         addPig();
         addDescription();
         addPayButton();
 
         return mRootView;
+    }
+
+    private void calculatePigWidth() {
+        Size screenSize = ScreenUtil.getScreenSize(this);
+        int width = (int)(screenSize.getWidth()*0.4f);
+
+        mPigSize = new Size(width, screenSize.getHeight());
+    }
+
+    private void calculatePayBtnSize() {
+        int size = (int) getResources().getDimension(R.dimen.payment_activity__btn_pay_size);
+
+        mBtnPaySize = new Size(size, size);
+    }
+
+    private void calculateDescriptionSize() {
+        Size screenSize = ScreenUtil.getScreenSize(this);
+        int width = screenSize.getWidth()-mPigSize.getWidth();
+
+        int btnPayMargin = (int) getResources().getDimension(R.dimen.payment_activity__btn_pay_margin);
+        int height = screenSize.getHeight() - mBtnPaySize.getHeight() - 2*btnPayMargin;
+
+        mDescriptionSize = new Size(width, height);
+    }
+
+    private void calculatePayBtnMargin() {
+        int left = mPigSize.getWidth() + (mDescriptionSize.getWidth() - mBtnPaySize.getWidth())/2;
+
+        int btnPayMargin = (int) getResources().getDimension(R.dimen.payment_activity__btn_pay_margin);
+        int top = mDescriptionSize.getHeight() + btnPayMargin;
+
+        mBtnPayMargin = new Margin(left, top, 0,0);
     }
 
     private void addBackground() {
@@ -129,11 +202,52 @@ public class PaymentActivity extends DaggerAppCompatActivity {
     }
 
     private void addPig() {
-        // TODO
+        mImagePig = new ImageView(this);
+        mImagePig.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        mImagePig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPayment();
+            }
+        });
+
+        mImageLoaderService.loadAssets(ASSETS_PIG, mPigSize.getWidth(), mPigSize.getWidth(), mImagePig);
+
+        mRootView.addView(mImagePig, LayoutHelper.createFramePx(mPigSize, Gravity.START | Gravity.TOP));
     }
 
     private void addDescription() {
-        // TODO
+        mImageDescription = new ImageView(this);
+        mImageDescription.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        mImageDescription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPayment();
+            }
+        });
+
+        String assetsPath = getDescriptionAssetsPath();
+        mImageLoaderService.loadAssets(assetsPath, mDescriptionSize.getWidth(), mDescriptionSize.getHeight(), mImageDescription);
+
+        mRootView.addView(mImageDescription, LayoutHelper.createFramePx(mDescriptionSize, Gravity.START | Gravity.TOP, mPigSize.getWidth(), 0, 0, 0));
+    }
+
+    private String getDescriptionAssetsPath() {
+        Locale locale = getResources().getConfiguration().locale;
+        switch (locale.getLanguage()) {
+            case "ru":
+                return ASSETS_DESCRIPTION_RU;
+            default:
+                return ASSETS_DESCRIPTION_EN;
+        }
+    }
+
+    private String getBtnPayText() {
+//        Locale locale = getResources().getConfiguration().locale;
+//        Currency currency = Currency.getInstance(locale);
+//        return currency.getCurrencyCode();
+
+        return "3 $";
     }
 
     private void addPayButton() {
@@ -147,12 +261,25 @@ public class PaymentActivity extends DaggerAppCompatActivity {
             }
         });
 
-        mRootView.addView(mBtnPay, LayoutHelper.createFramePx(mBtnSize, Gravity.END | Gravity.TOP));
+        TextView textView = new TextView(this);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 32);
+        textView.setTypeface(null, Typeface.BOLD);
+        textView.setLines(1);
+        textView.setAllCaps(true);
+        textView.setTextColor(Color.BLACK);
+        textView.setGravity(Gravity.CENTER);
+        textView.setText(getBtnPayText());
+
+        FrameLayout frameLayout = new FrameLayout(this);
+        frameLayout.addView(mBtnPay, LayoutHelper.createFramePx(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        frameLayout.addView(textView, LayoutHelper.createFramePx(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, Gravity.CENTER));
+
+        mRootView.addView(frameLayout, LayoutHelper.createFramePx(mBtnPaySize, Gravity.START | Gravity.TOP, mBtnPayMargin));
     }
 
     private Drawable createBtnPayDrawable() {
-        Bitmap pressed = mImageLoaderService.getAssets(ASSETS_BTN_PAY_PRESSED, mBtnSize.getWidth(), mBtnSize.getHeight());
-        Bitmap normal = mImageLoaderService.getAssets(ASSETS_BTN_PAY_NORMAL, mBtnSize.getWidth(), mBtnSize.getHeight());
+        Bitmap pressed = mImageLoaderService.getAssets(ASSETS_BTN_PAY_PRESSED, mBtnPaySize.getWidth(), mBtnPaySize.getHeight());
+        Bitmap normal = mImageLoaderService.getAssets(ASSETS_BTN_PAY_NORMAL, mBtnPaySize.getWidth(), mBtnPaySize.getHeight());
 
         StateListDrawable drawable = new StateListDrawable();
         drawable.addState(new int[]{android.R.attr.state_pressed}, new BitmapDrawable(getResources(), pressed));
@@ -225,7 +352,7 @@ public class PaymentActivity extends DaggerAppCompatActivity {
     }
 
     private void handleError(int statusCode) {
-        Log.w("loadPaymentData failed", String.format("Error code: %d", statusCode));
+        Log.e("loadPaymentData failed", String.format("Error code: %d", statusCode));
     }
 
     // endregion
